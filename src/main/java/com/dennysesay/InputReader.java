b224f34d07394bfb52961abd0285a575d5149ff5
Denny Sesay <79401100+DennySesay@users.noreply.github.com>
2026-05-24 03:34:10 +0200

    Delete src/main/java/com/dennysesay directory

--- src/main/java/com/dennysesay/InputReader.java
+++
@@ -1,7 +0,0 @@
-package com.dennysesay;
-
-import java.util.Scanner;
-
-public class InputReader {
-    private Scanner input = new Scanner(System.in);
-}
--- src/main/java/com/dennysesay/folder/FeatureFolder.java
+++
@@ -1,5 +0,0 @@
-package com.dennysesay.folder;
-
-public class FeatureFolder {
-
-}
